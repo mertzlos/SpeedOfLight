23bf4e0929fd95dd930aecfc3e16a9c752c4df33
antreasmertzelos <a.mertzelos@gmail.com>
2016-11-20 01:34:31 +0200

    test

--- app/src/main/java/com/mertzlos/speedoflight/MainActivity.java
+++ app/src/main/java/com/mertzlos/speedoflight/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
+        //test
 
         Intent intent = getIntent();
         if(null != intent){
